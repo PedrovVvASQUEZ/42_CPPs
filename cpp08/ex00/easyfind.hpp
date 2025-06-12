72fc7333b189cf8155500a33cedc0aae05dd9339
Pierre Grellier-beloqui <pgrellie@f2r8s17.clusters.42paris.fr>
2025-06-12 18:37:48 +0200

    jijiji

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgrellie <pgrellie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 17:51:38 by pgrellie          #+#    #+#             */
+/*   Updated: 2025/06/12 18:36:47 by pgrellie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int to_find)
+{
+	
+}
+
+#endif
