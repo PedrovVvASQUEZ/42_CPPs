f8432310abf03da1b0c6ff0b069c7115c307e30d
PedrovVvASQUEZ <152516991+PedrovVvASQUEZ@users.noreply.github.com>
2025-06-16 23:23:59 +0000

    cmon

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -11,4 +11,65 @@
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include "stdexcept"
 
+//==================== Contructors N Destructor ===================//
+
+Span::Span(void) : _vec()
+{
+	return ;
+}
+
+Span::Span(unsigned int N) : _vec(N, 0)
+{
+	return ;
+}
+
+Span::Span(const Span &rhs) : _vec(rhs._vec)
+{
+	return ;
+}
+
+Span::~Span(void)
+{
+	return ;
+}
+
+//=============== Operators Overload Member Functions =============//
+
+Span	&Span::operator=(const Span &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_vec = rhs._vec;
+	}
+	return (*this);
+}
+
+//======================= Member functions ========================//
+
+void	Span::addNumber(int N)
+{
+	if (this->_vec.size() >= this->_vec.capacity())
+		throw std::runtime_error("Span is full");
+	this->_vec.push_back(N);
+}
+
+void	Span::fillVec(unsigned int N)
+{
+	if (N > this->_vec.capacity())
+		throw std::runtime_error("cannot fill more than capacity");
+	while (this->_vec.size() < N)
+		this->addNumber(std::rand());
+}
+
+std::vector<unsigned int>::iterator	Span::shortestSpan(std::vector<unsigned int> &vec) const
+{
+	
+}
+
+
+std::vector<unsigned int>::iterator Span::longestSpan(std::vector<unsigned int> &vec) const
+{
+	
+}
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -13,4 +13,29 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <vector>
+# include <cstdlib>
+# include <ctime>
+
+class	Span{
+
+public:
+
+	Span(unsigned int N);
+	Span(const Span &rhs);
+	~Span(void);
+
+	Span	&operator=(const Span &rhs);
+
+	void								addNumber(int N);
+	void								fillVec(unsigned int N);
+	std::vector<unsigned int>::iterator	shortestSpan(std::vector<unsigned int> &vec) const;
+	std::vector<unsigned int>::iterator longestSpan(std::vector<unsigned int> &vec) const;
+
+private:
+
+	Span(void);
+	std::vector<unsigned int>	_vec;
+};
+
 #endif
