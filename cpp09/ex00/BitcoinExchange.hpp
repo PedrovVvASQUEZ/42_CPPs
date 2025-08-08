757947557a755ac67e051c2eade8f0f2ec1f8c0b
Pierre Grellier-beloqui <pgrellie@f2r7s12.clusters.42paris.fr>
2025-08-08 18:44:33 +0200

    ohjrgoubeg

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: pgrellie <pgrellie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 16:35:25 by pgrellie          #+#    #+#             */
-/*   Updated: 2025/08/07 18:46:22 by pgrellie         ###   ########.fr       */
+/*   Updated: 2025/08/08 17:36:52 by pgrellie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
-# include <algorithm>
 # include <map>
+# include <fstream>
+# include <sstream>
 # include <string>
+# include <exception>
 
 
 
@@ -25,13 +27,41 @@ class BitcoinExchange{
 public:
 
 	BitcoinExchange(void);
-	BitcoinExchange();
 	BitcoinExchange(const BitcoinExchange &rhs);
 	~BitcoinExchange(void);
+	
+	BitcoinExchange	&operator=(const BitcoinExchange &rhs);
+	
 
 private:
 
-	std::map<>	_map;
+	std::map<std::string, double>	_chart;
 };
 
 #endif
+
+// class BitcoinExchange {
+// public:
+//     // Constructeurs (supprimez le doublon)
+//     BitcoinExchange(void);
+//     BitcoinExchange(const BitcoinExchange &rhs);
+//     ~BitcoinExchange(void);
+    
+//     BitcoinExchange &operator=(const BitcoinExchange &rhs);
+    
+//     // Méthodes principales
+//     void loadDatabase(const std::string &filename);
+//     void processInput(const std::string &filename);
+//     double getPrice(const std::string &date) const;
+    
+//     // Validation
+//     bool isValidDate(const std::string &date) const;
+//     bool isValidValue(double value) const;
+    
+// private:
+//     std::map<std::string, double> _chart;
+    
+//     // Méthodes utilitaires
+//     std::string trim(const std::string &str) const;
+//     void parseLine(const std::string &line, std::string &date, double &value) const;
+// };
